ee03d1ea1545487d3aacf6dbb75de0026f5f66aa
anastasiya <anastasiya.kovpak@gmail.com>
2022-11-12 20:49:38 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,6 @@
+﻿// Нахождение квадрата числа, введенного пользователем
+Console.Write("Введите целое число и нажмите Enter: ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+int result = number * number;
+Console.WriteLine($"{number} -> {result}");
