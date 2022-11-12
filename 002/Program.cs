8c8eb5c4570e364d091a995f7df3cde9b9d52670
anastasiya <anastasiya.kovpak@gmail.com>
2022-11-12 21:05:24 +0300

    002

---
+++ 002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Проверка, является ли первое число квадратом второго
+Console.Write("Введите первое число и нажмите Enter: ");
+int numberOne = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число и нажмите Enter: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+int numberSqr = numberTwo*numberTwo;
+if (numberSqr == numberOne) 
+{
+    Console.Write("Первое число является квадратом второго");
+}
+else
+{
+    Console.Write("Первое число НЕ является квадратом второго");
+}
