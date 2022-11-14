e0c3263d07bc6768215e407487bff93bc982102f
anastasiya <anastasiya.kovpak@gmail.com>
2022-11-14 18:45:48 +0300

    Zadacha4

--- HW001/Program.cs
+++ HW001/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.Write("Введите первое число и нажмите Enter: ");
 int numberOne = int.Parse(Console.ReadLine());
-
 Console.Write("Введите второе число и нажмите Enter: ");
 int numberTwo = int.Parse(Console.ReadLine());
+
 
 
 if (numberOne > numberTwo)
